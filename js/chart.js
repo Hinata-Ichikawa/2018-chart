e78c7cea934a2f8edd854c6b5e0644ec717b1355
市川ひなた <hinakami0701@gmail.com>
2018-04-19 11:38:28 +0900

    スライダーのスクロールはできた。ただ、初期ページに一度戻ってしまう。

--- js/chart.js
+++ js/chart.js
@@ -1,17 +1,12 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-      keyboardScrolling: false,
-      recordHistory: false,
+      // keyboardScrolling: false,
+      anchors: ['page1', 'page2', 'page3', 'page4'],
+      resize : false,
       sectionsColor: ['#AACF52', '#4BBFC3', '#7BAABE', '#E5E5E5']
     });
 });
 
 $(document).on('click', '#down', function(){
-  console.log("section");
-  $.fn.fullpage.moveSectionDown();
-});
-
-$(document).on('click', '#last', function(){
-  console.log("section");
   $.fn.fullpage.moveSectionDown();
 });
